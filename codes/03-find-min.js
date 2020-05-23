3b89644d054ac25b88ab95b9e83512a348c22015
bruski <bruski@qq.com>
2020-05-23 18:34:56 +0800

    feat: 提交作业

---
+++ codes/03-find-min.js
@@ -0,0 +1,4 @@
+var arr = [12, 34, 32, 89, 4];
+
+// 答案
+const [min] = arr.sort((a, b) => a - b);
---
+++ codes/09-promise-refactor.js
@@ -0,0 +1,24 @@
+// Promise优化
+
+const promise1 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("hello");
+  }, 10);
+});
+
+const promise2 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("lagou");
+  }, 10);
+});
+
+const promise3 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("I ❤️ U");
+  }, 10);
+});
+
+Promise.all([promise1, promise2, promise3]).then((res) => {
+  const [a, b, c] = res;
+  console.log(a + b + c);
+});
